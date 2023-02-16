6a4bee0237d397ab47236fb70610b3c36ef85069
InsunBaek <xecell515@gmail.com>
2023-02-16 21:10:46 +0900

    movie with css

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -1,19 +1,23 @@
 import PropTypes from "prop-types"
 import { Link } from "react-router-dom"
+import styles from "./Movie.module.css"
 
-const Movie = ({ id, coverImg, title, summary, genres }) => {
+const Movie = ({ id, coverImg, title, year, summary, genres }) => {
     return (
-        <div>
-            <img src={coverImg} alt={title} />
-            <h2>
-                <Link to={`/movie/${id}`}>{title}</Link>
-            </h2>
-            <p>{summary}</p>
-            <ul>
-                {genres.map((genre) => (
-                    <li key={genre}>{genre}</li>
-                ))}
-            </ul>
+        <div className={styles.movie}>
+            <img src={coverImg} alt={title} className={styles.movie__img} />
+            <div>
+                <h2 className={styles.movie__title}>
+                    <Link to={`/movie/${id}`}>{title}</Link>
+                </h2>
+                <h3 className={styles.movie__year}>{year}</h3>
+                <p>{summary.length > 235 ? `${summary.slice(0, 235)}...` : summary}</p>
+                <ul>
+                    {genres.map((genre) => (
+                        <li key={genre}>{genre}</li>
+                    ))}
+                </ul>
+            </div>
         </div>
     )
 }
@@ -22,7 +26,7 @@ Movie.propTypes = {
     id: PropTypes.number.isRequired,
     coverImg: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
-    summaray: PropTypes.string.isRequired,
+    summary: PropTypes.string.isRequired,
     genres: PropTypes.arrayOf(PropTypes.string).isRequired,
 }
 
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
 import Movie from '../components/Movie'
+import styles from "./Home.module.css";
 
 const Home = () => {
     const [loading, setLoading] = useState(true)
@@ -19,16 +20,17 @@ const Home = () => {
     }, [])
 
     return (
-        <div>
+        <div className={styles.container}>
             {loading ? (<h1>Loading...</h1>) : (
-                <div>                    
+                <div className={styles.movies}>     
                     {movies.map((movie, index) => (
                         <Movie
                             key={movie.id}
                             id={movie.id}
+                            year={movie.year}
                             coverImg={movie.medium_cover_image}
                             title={movie.title}
-                            summaray={movie.summary}
+                            summary={movie.summary}
                             genres={movie.genres}
                         />
                     ))}
